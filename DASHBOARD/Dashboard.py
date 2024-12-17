536dfcb81cc3bfb1ab4fb51269380493fcbb81ec
Mubarok982 <rizqymubarok99@gmail.com>
2024-12-17 22:02:38 +0700

    Update Dashboard.py

--- DASHBOARD/Dashboard.py
+++ DASHBOARD/Dashboard.py
@@ -9,11 +9,11 @@ import streamlit.components.v1 as components
 
 #load data 
 # Data import dan processing 
-customer_df = pd.read_csv("main_data.csv")
-geolocation_df = pd.read_csv("main_data.csv")
-order_payments_df = pd.read_csv("main_data.csv")
+customer_df = pd.read_csv("DASHBOARD\main_data.csv")
+geolocation_df = pd.read_csv("DASHBOARD\main_data.csv")
+order_payments_df = pd.read_csv("DASHBOARD\main_data.csv")
 
-order_purchase = pd.read_csv("main_data.csv")
+order_purchase = pd.read_csv("DASHBOARD\main_data.csv")
 # Menampilkan judul di Sidebar
 st.sidebar.title("Analisis Data Pelanggan, Pembayaran, dan Geolokasi")
 st.sidebar.image("c:\SINAU\DATA ANALIS\dicoding\SUBMISSION\Image.png")
